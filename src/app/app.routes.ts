9482d2f2ed1c3f08c20cb4b19f24473bdcd84b1e
krisnag <k_gusti@hotmail.com>
2025-07-11 06:44:55 +0000

    Updated to new home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,10 +6,6 @@ export const routes: Routes = [
         loadComponent: () => import('./home/home').then(m => m.Home),
     },
     {
-        path: 'home/:id',
-        loadComponent: () => import('./detail/detail').then(m => m.Detail),
-    },
-    {
         path: '',
         pathMatch: 'full',
         redirectTo: 'home',
--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,18 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { 
-  IonContent, 
-  IonHeader, 
-  IonIcon, 
-  IonItem, 
-  IonLabel, 
-  IonList, 
-  IonNote, 
-  IonTitle, 
+import {
+  IonContent,
+  IonHeader,
+  IonIcon,
+  IonLabel,
+  IonTitle,
   IonToolbar,
 } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import { logoIonic } from 'ionicons/icons';
+import {
+  logoIonic,
+  pricetagOutline,
+  addCircleOutline,
+  createOutline,
+  trendingUpOutline,
+  refreshOutline,
+  downloadOutline
+} from 'ionicons/icons';
 
 @Component({
   selector: 'app-home',
@@ -21,20 +26,23 @@ import { logoIonic } from 'ionicons/icons';
     IonToolbar,
     IonTitle,
     IonContent,
-    IonList,
-    IonItem,
     IonIcon,
     IonLabel,
-    IonNote,
     RouterLink
   ],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
 export class Home {
-  items = new Array(50).fill(1);
-  
   constructor() {
-    addIcons({ logoIonic })
+    addIcons({
+      logoIonic,
+      pricetagOutline,
+      addCircleOutline,
+      createOutline,
+      trendingUpOutline,
+      refreshOutline,
+      downloadOutline
+    });
   }
 }
